790e70e86c143e62bcd80a79bd4cbb1534319c1a
Durjoysarker-swarup <durjoysarker78@gmail.com>
2025-10-30 03:00:37 +0600

    This is exception learning

---
+++ Exception.py
@@ -0,0 +1,61 @@
+'''
+print("hello, world)
+#SyntaxError need to fix by me
+'''
+
+'''
+x = int(input("what's x? "))
+print(f"x is {x}")
+#what's still go wrong
+# If the person give the string it's an error. it si a ValurError
+# So now need to fix this.
+# try and excpet can do this
+'''
+
+'''
+# Handle this value error
+while True:
+    try:
+        x = int(input("what's x? "))
+    except ValueError:
+        print("x is not an integer")
+    else:
+        break
+print (f"x is {x}")
+
+# Other ways
+while True:
+    try:
+        x = int(input("what's x? "))
+        break
+    except ValueError:
+        print("x is not an integer")
+print (f"x is {x}")
+'''
+
+def main():
+    x = get_int("what is x? ")
+    print(f"x is {x}")    
+    
+def get_int(prompt):
+    while True:
+        try:
+            x = int(input(prompt))
+            return x
+        except ValueError:
+            print("x is not an integer")
+            #you can use pass. Where you don't tell the user again and agian that this is not an integer.
+            
+    
+
+
+
+
+
+
+
+
+
+
+
+
